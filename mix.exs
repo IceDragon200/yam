8187ec5d3494e36e1b32ef53f899b01e4bccbb3a
Corey Powell <mistdragon100@gmail.com>
2021-07-13 20:03:26 -0500

    Support decimal 2.0 and bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Yam.MixProject do
   def project do
     [
       app: :yam,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,7 +21,7 @@ defmodule Yam.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:decimal, "~> 1.0"},
+      {:decimal, "~> 1.9 or ~> 2.0"},
     ]
   end
 end
